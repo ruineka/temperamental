9273ba9342e3a8c2fc0504dfde928510f34fb627
Matthew Anderson <ruinairas1992@gmail.com>
2023-01-07 19:39:43 -0600

    Messy work to a hopefully bright future

--- GUI.py
+++ GUI.py
@@ -8,8 +8,12 @@ import sys
 
 from profile import MIN_TDP,STEP_ONE,STEP_TWO,MAX_TDP
 
-SMT = ''
-BOOST = ''
+SMT = subprocess.getoutput('cat /sys/devices/system/cpu/smt/control')
+BOOST = subprocess.getoutput('cat /sys/devices/system/cpu/cpufreq/boost')
+
+currentTDP = 0
+targetTDP = 0
+defaultTDP = 0
 
 # Gui Elements
 pygame.init()
@@ -21,121 +25,135 @@ background = pygame.Surface((800,600))
 manager = pygame_gui.UIManager((800,600))
 
 # Buttons
-button1 = pygame_gui.elements.UIButton(
-                            relative_rect=pygame.Rect(0,200,100,100),
+MIN_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(0,150,100,50),
                             text=str(int(MIN_TDP) // 1000) + "W TDP",
                             manager=manager
                         )
-button2 = pygame_gui.elements.UIButton(
-                            relative_rect=pygame.Rect(0,300,100,100),
+TIER1_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(100,150,100,50),
                             text=str(int(STEP_ONE) // 1000) + "W TDP",
                             manager=manager
                         )
-button3 = pygame_gui.elements.UIButton(
-                            relative_rect=pygame.Rect(0,400,100,100),
+TIER2_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(200,150,100,50),
                             text=str(int(STEP_TWO) // 1000) + "W TDP",
                             manager=manager
                         )
-button4 = pygame_gui.elements.UIButton(
-                            relative_rect=pygame.Rect(0,500,100,100),
+MAX_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(300,150,100,50),
                             text=str(int(MAX_TDP) // 1000) + "W TDP",
                             manager=manager
                         )
-button5 = pygame_gui.elements.UIButton(
-                            relative_rect=pygame.Rect(100,510,175,30),
+DEFAULT_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(475,560,175,40),
                             text='Restore Defaults',
                             manager=manager
                         )
-button6 = pygame_gui.elements.UIButton(
-                            relative_rect=pygame.Rect(100,570,175,30),
+SMT_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(0,570,175,30),
                             text='Toggle SMT',
                             manager=manager
                         )
-button7 = pygame_gui.elements.UIButton(
-                            relative_rect=pygame.Rect(100,540,175,30),
+BOOST_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(0,540,175,30),
                             text='Toggle CPU Boost',
                             manager=manager
                         )
-button8 = pygame_gui.elements.UIButton(
-                            relative_rect=pygame.Rect(550,250,175,30),
+PERFORMANCE_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(0,510,175,30),
                             text='Toggle Performance Mode',
                             manager=manager
                         )
-button9 = pygame_gui.elements.UIButton(
-                            relative_rect=pygame.Rect(550,200,175,30),
+POWERSAVER_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(0,480,175,30),
                             text='Toggle Power Saver Mode',
                             manager=manager
                         )
-button10 = pygame_gui.elements.UIButton(
-                            relative_rect=pygame.Rect(700,5,70,30),
+CLOSE_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(405,560,70,40),
                             text='Close',
                             manager=manager
                         )
+SAVE_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(335,560,70,40),
+                            text='Save',
+                            manager=manager
+                        )
+LOAD_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(250,560,70,40),
+                            text='Load',
+                            manager=manager
+                        )
+POSITIVE_INCREMENT_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(0,200,75,40),
+                            text='+',
+                            manager=manager
+                        )
+NEGATIVE_INCREMENT_BUTTON = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(75,200,75,40),
+                            text='-',
+                            manager=manager
+                        )
+APPLY_CHANGES = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(650,560,150,40),
+                            text='Apply Changes',
+                            manager=manager
+                        )
+QUIET_FAN= pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(600,260,175,40),
+                            text='Quiet Fan',
+                            manager=manager
+                        )
+BALANCED_FAN = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(600,300,175,40),
+                            text='Balanced Fan',
+                            manager=manager
+                        )
+PERFORMANCE_FAN = pygame_gui.elements.UIButton(
+                            relative_rect=pygame.Rect(600,340,175,40),
+                            text='Performance Fan',
+                            manager=manager
+                        )
 # HUD information
-label1 = pygame_gui.elements.UILabel(
-                            relative_rect=pygame.Rect(100,350,225,100),
-                            text="Current Power Draw:" + " " + subprocess.getoutput("awk '{print $1*10^-5 " "}' /sys/class/power_supply/BATT/current_now") + "W",
+CURRENT_TDP_VALUE = pygame_gui.elements.UITextBox(
+                            relative_rect=pygame.Rect(550,500,225,50),
+                            html_text="Current TDP Target: ",
                             manager=manager
 )
-label2 = pygame_gui.elements.UILabel(
-                            relative_rect=pygame.Rect(100,375,225,100),
-                            text="Current TDP Value:" + " " + "Not Set",
-                            manager=manager
-)
-label3 = pygame_gui.elements.UILabel(
-                            relative_rect=pygame.Rect(100,400,200,100),
+BOOST_LABEL = pygame_gui.elements.UILabel(
+                            relative_rect=pygame.Rect(600,150,150,50),
                             text=BOOST,
                             manager=manager
 )
-label4 = pygame_gui.elements.UILabel(
-                            relative_rect=pygame.Rect(100,425,200,100),
+SMT_LABEL = pygame_gui.elements.UILabel(
+                            relative_rect=pygame.Rect(600,200,150,50),
                             text=SMT,
                             manager=manager
 )
-label5 = pygame_gui.elements.UILabel(
-                            relative_rect=pygame.Rect(14,0,500,100),
-                            text="CPU Name: " + subprocess.check_output("lscpu | grep \"Model name\" | cut -d : -f 2 | xargs", shell=True, universal_newlines=True).strip(),
-                            manager=manager
-)
-label11 = pygame_gui.elements.UILabel(
-                            relative_rect=pygame.Rect(550,0,225,100),
+POWER_PROFILE_LABEL = pygame_gui.elements.UILabel(
+                            relative_rect=pygame.Rect(0,75,225,100),
                             text="Power Profile: Not Enabled",
                             manager=manager
 )
-label12 = pygame_gui.elements.UILabel(
-                            relative_rect=pygame.Rect(514,25,225,100),
+FAN_CURVE_LABEL = pygame_gui.elements.UILabel(
+                            relative_rect=pygame.Rect(250,75,225,100),
                             text="Fan Curve: Not Implemented",
                             manager=manager
 )
-label13 = pygame_gui.elements.UILabel(
-                            relative_rect=pygame.Rect(527,50,225,100),
+HHFC_LABEL = pygame_gui.elements.UILabel(
+                            relative_rect=pygame.Rect(600,75,225,100),
                             text="",
                             manager=manager
 )
-label14 = pygame_gui.elements.UILabel(
-                            relative_rect=pygame.Rect(523,75,225,100),
+RYZENADJ_STATUS_LABEL = pygame_gui.elements.UILabel(
+                            relative_rect=pygame.Rect(450,75,225,100),
                             text="Ryzenadj: Not Found",
                             manager=manager
 )
-label15 = pygame_gui.elements.UILabel(
-                            relative_rect=pygame.Rect(225,-25,350,100),
-                            text="System Name: " + subprocess.getoutput('cat /sys/devices/virtual/dmi/id/product_name'),
-                            manager=manager
-)
-textbox1 = pygame_gui.elements.UITextBox(
-                            relative_rect=pygame.Rect(350,400,425,200),
-                            html_text="Use this tool at your own risk, I don't take responsiblity for any issues caused by this tool",
+TEXT_FIELD_SUMMARY = pygame_gui.elements.UITextBox(
+                            relative_rect=pygame.Rect(0,0,800,100),
+                            html_text="System Name: " + subprocess.getoutput('cat /sys/devices/virtual/dmi/id/product_name') + "\n" +"CPU Name: " + subprocess.check_output("lscpu | grep \"Model name\" | cut -d : -f 2 | xargs", shell=True, universal_newlines=True).strip(),
                             manager=manager
 )
 
-        # Update Hud Values       
-label1.set_text("Current Power Draw" + " " + subprocess.getoutput("awk '{print $1*10^-5 " "}' /sys/class/power_supply/BATT/current_now") + "W")
-        
-if subprocess.getoutput('cat /sys/devices/system/cpu/smt/control') == 'on':
-   label4.set_text("SMT Enabled")
-else:
-   label4.set_text("SMT Disabled")
-if subprocess.getoutput('cat /sys/devices/system/cpu/cpufreq/boost') == '1':
-   label3.set_text("Boost Enabled")
-else:
-   label3.set_text("Boost Disabled")
--- temperamental.py
+++ temperamental.py
@@ -8,17 +8,16 @@ import subprocess
 import sys
 import GUI
 import profile
+import vlc
+import time
 from os import path
 from profile import MIN_TDP,STEP_ONE,STEP_TWO,MAX_TDP
 
 # Define variables
 
-currentTDP = 0
-targetTDP = 0
-defaultTDP = 0
-
 RYZENADJ='/usr/bin/ryzenadj'
 PARAMS=''
+GUI.currentTDP = int(MIN_TDP) // 1000
 
 # Grab current values before changes
 DEFAULT_SMT = subprocess.getoutput('cat /sys/devices/system/cpu/smt/control')
@@ -35,9 +34,9 @@ for joystick in joysticks:
 # Check for dependencies
 if path.exists("/usr/bin/ryzenadj"):
    print("Ryzenadj Found")
-   GUI.label14.set_text("Ryzenadj: Found")
+   GUI.RYZENADJ_STATUS_LABEL.set_text("Ryzenadj: Found")
 else:
-   GUI.label14.set_text("Ryzenadj: Not Found")
+   GUI.RYZENADJ_STATUS_LABEL.set_text("Ryzenadj: Not Found")
    
 if path.exists("/usr/share/handycon.py"):
    print("HandyGCCS Found")
@@ -46,10 +45,10 @@ else:
    
 if path.exists("/usr/share/hhfc/"):
    print("HHFC Found")
-   GUI.label13.set_text("HHFC: Found")
+   GUI.HHFC_LABEL.set_text("HHFC: Found")
 else:
    print("HHFC Not Found")
-   GUI.label13.set_text("HHFC: Not Found")
+   GUI.HHFC_LABEL.set_text("HHFC: Not Found")
    
 isRunning = True
 clock = pygame.time.Clock()
@@ -58,7 +57,7 @@ while isRunning:
 
     time_delta = clock.tick(60)/1000.0
     GUI.window_surface.blit(GUI.background,(0,0))
-    GUI.window_surface.fill((64,64,64))
+    GUI.window_surface.fill((0,10,25))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -67,7 +66,7 @@ while isRunning:
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
             
-                if event.ui_element == GUI.button6:
+                if event.ui_element == GUI.SMT_BUTTON:
 
                     if subprocess.getoutput('cat /sys/devices/system/cpu/smt/control') == 'on':
                        SMT = 'off'
@@ -75,48 +74,70 @@ while isRunning:
                        SMT = 'on'
                     subprocess.run('/usr/bin/echo ' + SMT + ' > /sys/devices/system/cpu/smt/control', shell=True)
             
-                if event.ui_element == GUI.button1:
-                    PARAMS="--stapm-limit=" + MIN_TDP + " " + "--fast-limit=" + MIN_TDP + " " + " --slow-limit=" + MIN_TDP + " " + " --tctl-temp=90"
-                    subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)
-                    GUI.label2.set_text("Current TDP Value:" + " " + str(int(MIN_TDP) // 1000) +"W TDP")
+                if event.ui_element == GUI.MIN_BUTTON:
+                    PARAMS="--stapm-limit=" + MIN_TDP + " " + "--fast-limit=" + MIN_TDP + " " + "--slow-limit=" + MIN_TDP + " " + "--tctl-temp=90"
+                    GUI.currentTDP = int(MIN_TDP) // 1000
+                    GUI.CURRENT_TDP_VALUE.set_text("Current TDP Value:" + " " + str(int(MIN_TDP) // 1000) +"W TDP")
                     
-                if event.ui_element == GUI.button2:
-                    PARAMS="--stapm-limit=" + STEP_ONE + " " + "--fast-limit=" + STEP_ONE + " " + " --slow-limit=" + STEP_ONE + " " + " --tctl-temp=90"
-                    subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)
-                    GUI.label2.set_text("Current TDP Value:" + " " + str(int(STEP_ONE) // 1000) +"W TDP")
+                if event.ui_element == GUI.TIER1_BUTTON:
+                    PARAMS="--stapm-limit=" + STEP_ONE + " " + "--fast-limit=" + STEP_ONE + " " + "--slow-limit=" + STEP_ONE + " " + "--tctl-temp=90"
+                    GUI.currentTDP = int(STEP_ONE) // 1000
+                    GUI.CURRENT_TDP_VALUE.set_text("Current TDP Value:" + " " + str(int(STEP_ONE) // 1000) +"W TDP")
                     
-                if event.ui_element == GUI.button3:
-                    PARAMS="--stapm-limit=" + STEP_TWO + " " + "--fast-limit=" + STEP_TWO + " " + " --slow-limit=" + STEP_TWO + " " + " --tctl-temp=90"
-                    subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)   
-                    GUI.label2.set_text("Current TDP Value:" + " " + str(int(STEP_TWO) // 1000) +"W TDP")  
+                if event.ui_element == GUI.TIER2_BUTTON:
+                    PARAMS="--stapm-limit=" + STEP_TWO + " " + "--fast-limit=" + STEP_TWO + " " + "--slow-limit=" + STEP_TWO + " " + "--tctl-temp=90"
+                    GUI.currentTDP = int(STEP_TWO) // 1000
+                    GUI.CURRENT_TDP_VALUE.set_text("Current TDP Value:" + " " + str(int(STEP_TWO) // 1000) +"W TDP")  
                          
-                if event.ui_element == GUI.button4:
-                    PARAMS="--stapm-limit=" + MAX_TDP + " " + "--fast-limit=" + MAX_TDP + " " + " --slow-limit=" + MAX_TDP + " " + " --tctl-temp=90"
-                    subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)
-                    GUI.label2.set_text("Current TDP Value:" + " " + str(int(MAX_TDP) // 1000) +"W TDP")
-                if event.ui_element == GUI.button7:
+                if event.ui_element == GUI.MAX_BUTTON:
+                    PARAMS="--stapm-limit=" + MAX_TDP + " " + "--fast-limit=" + MAX_TDP + " " + "--slow-limit=" + MAX_TDP + " " + "--tctl-temp=90"
+                    GUI.currentTDP = int(MAX_TDP) // 1000
+                    GUI.CURRENT_TDP_VALUE.set_text("Current TDP Value:" + " " + str(int(MAX_TDP) // 1000) +"W TDP")
+                if event.ui_element == GUI.BOOST_BUTTON:
                     if subprocess.getoutput('cat /sys/devices/system/cpu/cpufreq/boost') == '1':
                        BOOST = '0'
                     else:
                        BOOST = '1'
                     subprocess.run('/usr/bin/echo ' +  BOOST + ' > /sys/devices/system/cpu/cpufreq/boost', shell=True)
                     
-                if event.ui_element == GUI.button5:
+                if event.ui_element == GUI.DEFAULT_BUTTON:
                    subprocess.run('/usr/bin/echo ' +  DEFAULT_BOOST + ' > /sys/devices/system/cpu/cpufreq/boost', shell=True)
                    subprocess.run('/usr/bin/echo ' +  DEFAULT_SMT + ' > /sys/devices/system/cpu/smt/control', shell=True)
                    
-                if event.ui_element == GUI.button8:
+                if event.ui_element == GUI.PERFORMANCE_BUTTON:
                     PARAMS='--max-performance'
                     subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)
-                    GUI.label11.set_text("Power Profile: Performance")
+                    GUI.POWER_PROFILE_LABEL.set_text("Power Profile: Performance")
                     
-                if event.ui_element == GUI.button9:
+                if event.ui_element == GUI.POWERSAVER_BUTTON:
                     PARAMS='--power-saving'
                     subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)
-                    GUI.label11.set_text("Power Profile: Power Saver")
+                    GUI.POWER_PROFILE_LABEL.set_text("Power Profile: Power Saver")
                     
-                if event.ui_element == GUI.button10:
+                if event.ui_element == GUI.CLOSE_BUTTON:
                     sys.exit(0)
+                if event.ui_element == GUI.POSITIVE_INCREMENT_BUTTON and GUI.currentTDP != int(MAX_TDP) // 1000:
+                    GUI.currentTDP = GUI.currentTDP + 1
+                    PARAMS="--stapm-limit=" + str(GUI.currentTDP * 1000) + " " + "--fast-limit=" + str(GUI.currentTDP * 1000) + " " + " --slow-limit=" + str(GUI.currentTDP * 1000) + " " + " --tctl-temp=90"
+                if event.ui_element == GUI.NEGATIVE_INCREMENT_BUTTON and GUI.currentTDP != int(MIN_TDP) // 1000:
+                    GUI.currentTDP = GUI.currentTDP - 1
+                    PARAMS="--stapm-limit=" + str(GUI.currentTDP * 1000) + " " + "--fast-limit=" + str(GUI.currentTDP * 1000) + " " + " --slow-limit=" + str(GUI.currentTDP * 1000) + " " + " --tctl-temp=90"
+                if event.ui_element == GUI.APPLY_CHANGES:
+                   print(RYZENADJ + " " + PARAMS)
+                   subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)
+                    
+# Update Hud Values   
+        if GUI.currentTDP != 0:
+           GUI.CURRENT_TDP_VALUE.set_text("Current TDP Target:" + " " + str(GUI.currentTDP) + "W")
+        
+        if GUI.SMT == "on":
+           GUI.SMT_LABEL.set_text("SMT Enabled")
+        else:
+           GUI.SMT_LABEL.set_text("SMT Disabled")
+        if GUI.BOOST == '1':
+           GUI.BOOST_LABEL.set_text("Boost Enabled")
+        else:
+           GUI.BOOST_LABEL.set_text("Boost Disabled")
                    
     # Gamepad inputs are yet to be implemented
         if event.type == pygame.JOYBUTTONDOWN:
