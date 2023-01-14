2c19533f3a4b3d6514a6b63773cbc8b1bc16adf4
Matthew Anderson <ruinairas1992@gmail.com>
2023-01-14 17:38:51 -0600

    Set permissions for boost/smt changing without prompt and gamepad button work

--- install.sh
+++ install.sh
@@ -11,5 +11,16 @@ cp -r profiles /usr/share/temperamental/
 cp -r temperamental-polkit-helpers/ /usr/bin/
 cp org.ruineka.temperamental.policy /usr/share/polkit-1/actions/
 
+
+# Set up the sudo permissions
+echo "***POSSIBLE SECURITY RISK. UNDERSTAND THE FOLLOWING***"
+echo "Adding password free sudo access to the following files for user ${SUDO_USER}:"
+echo "/sys/devices/system/cpu/cpufreq/boost"
+echo "/sys/devices/system/cpu/smt/control"
+cat <<-EOF > "/etc/sudoers.d/temperamental_sudo"
+${SUDO_USER} ALL=(ALL) NOPASSWD: /usr/bin/tee /sys/devices/system/cpu/cpufreq/boost*
+${SUDO_USER} ALL=(ALL) NOPASSWD: /usr/bin/tee /sys/devices/system/cpu/smt/control*
+EOF
+
 echo "Installed Successfully"
 
--- temperamental.py
+++ temperamental.py
@@ -13,11 +13,19 @@ from devices import MIN_TDP, STEP_ONE, STEP_TWO, MAX_TDP, QUIET_FAN_CONFIG, BALA
 
 # Define variables
 
-RYZENADJ = '/usr/bin/ryzenadj'
+RYZENADJ = '/usr/bin/temperamental-polkit-helpers/ryzenadj-polkit-helper'
 HHFC = 'hhfc -c'
 FAN_PROFILE_DIR = "/usr/share/temperamental/profiles/"
 PARAMS = ''
 GUI.currentTDP = int(MIN_TDP) // 1000
+MAX_PERFORMANCE="--max-performance"
+POWER_SAVER="--power-saving"
+STAPM_LIMIT="--stapm-limit"
+SLOW_LIMIT="--slow-limit"
+FAST_LIMIT="--fast-limit"
+TCTL_TEMP="--tctl-temp"
+TEMP_LIMIT="90"
+TARGET_TDP=MIN_TDP
 
 # Grab current values before changes
 DEFAULT_SMT = subprocess.getoutput('cat /sys/devices/system/cpu/smt/control')
@@ -29,10 +37,9 @@ subprocess.run(RYZENADJ + ' ' + '-i', shell=True)
 def get_profile():
     print("Grab profiles")
 
-
+    
 def set_tdp_values(button_number):
     global TARGET_TDP
-
     match button_number:
 
         case 0:
@@ -49,7 +56,7 @@ def set_tdp_values(button_number):
 
         case 2:
             TARGET_TDP = STEP_TWO
-            GUI.currentTDP = int(STEP_ONE) // 1000
+            GUI.currentTDP = int(STEP_TWO) // 1000
             GUI.CURRENT_TDP_VALUE.set_text(
                 "Current TDP Value:" + " " + str(int(STEP_ONE) // 1000) + "W TDP")
         case 3:
@@ -57,7 +64,12 @@ def set_tdp_values(button_number):
             GUI.currentTDP = int(MAX_TDP) // 1000
             GUI.CURRENT_TDP_VALUE.set_text(
                 "Current TDP Value:" + " " + str(int(MAX_TDP) // 1000) + "W TDP")
-
+        case 4:
+            TARGET_TDP = str(int(TARGET_TDP) + 1000)
+            GUI.currentTDP = TARGET_TDP
+        case 5:
+            TARGET_TDP = str(int(TARGET_TDP) - 1000)
+            GUI.currentTDP = TARGET_TDP
 
 def check_depends():
     # Check for dependencies
@@ -92,47 +104,70 @@ def gamepad_button_events(event):
     print("running gamepad events")
     print(event)
     if event.button == 0:
-        GUI.currentTDP = int(MIN_TDP) // 1000
+        button_number = 0
+        set_tdp_values(button_number)
     if event.button == 1:
-        sys.exit(0)
+        button_number = 1
+        set_tdp_values(button_number)
     if event.button == 2:
-        GUI.currentTDP = int(STEP_ONE) // 1000
+        button_number = 2
+        set_tdp_values(button_number)
     if event.button == 3:
-        GUI.currentTDP = int(MAX_TDP) // 1000
+        button_number = 3
+        set_tdp_values(button_number)
     if event.button == 4:
-        print("Toggle Boost")
+        if subprocess.getoutput('cat /sys/devices/system/cpu/cpufreq/boost') == '1':
+            GUI.BOOST = '0'
+        else:
+            GUI.BOOST = '1'
+        subprocess.run('/usr/bin/echo ' + GUI.BOOST +
+                       ' | sudo tee /sys/devices/system/cpu/cpufreq/boost', shell=True)
     if event.button == 5:
-        print("Toggle SMT")
-    if event.button == 13:
-        GUI.currentTDP = GUI.currentTDP + 1
-    if event.button == 14:
-        GUI.currentTDP = GUI.currentTDP - 1
-        print(event)
+        if subprocess.getoutput('cat /sys/devices/system/cpu/smt/control') == 'on':
+            GUI.SMT = 'off'
+        else:
+            GUI.SMT = 'on'
+        subprocess.run('/usr/bin/echo ' + GUI.SMT +
+                       ' | sudo tee /sys/devices/system/cpu/smt/control', shell=True)
+    if event.button == 6:
+        PARAMS = MAX_PERFORMANCE
+        subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)
+        GUI.POWER_PROFILE_LABEL.set_text("Power Profile: Performance")
+    if event.button == 7:
+        PARAMS = POWER_SAVER
+        subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)
+        GUI.POWER_PROFILE_LABEL.set_text("Power Profile: Power Saver")
+        
+    if event.button == 8:
+       sys.exit(0)
+    if event.button == 9:
+       print(RYZENADJ + ' ' + STAPM_LIMIT + "=" + TARGET_TDP + " " + FAST_LIMIT + "=" +
+             TARGET_TDP + " " + SLOW_LIMIT + "=" + TARGET_TDP + " " + TCTL_TEMP + "=" + TEMP_LIMIT)
+       subprocess.run(RYZENADJ + ' ' + STAPM_LIMIT + "=" + TARGET_TDP + " " + FAST_LIMIT + "=" +
+                      TARGET_TDP + " " + SLOW_LIMIT + "=" + TARGET_TDP + " " + TCTL_TEMP + "=" + TEMP_LIMIT, shell=True)
 
 
 def get_axis(event):
     hats = gamepad.get_numhats()
 
-    for i in range(hats):
-        hat = gamepad.get_hat(i)
+    #for i in range(hats):
+    #    hat = gamepad.get_hat(i)
 
-    if gamepad.get_hat(i) == (1, 0):
-        GUI.currentTDP = GUI.currentTDP + 1
-        print("right d pad")
-    if gamepad.get_hat(i) == (-1, 0):
-        GUI.currentTDP = GUI.currentTDP - 1
-        print("left d pad")
-
+    #if gamepad.get_hat(i) == (1, 0):
+    #    GUI.currentTDP = GUI.currentTDP + 1
+    #if gamepad.get_hat(i) == (-1, 0):
+    #    GUI.currentTDP = GUI.currentTDP - 1
 
 def keyboard_mouse_events(event):
 
     if event.ui_element == GUI.SMT_BUTTON:
         if subprocess.getoutput('cat /sys/devices/system/cpu/smt/control') == 'on':
-            SMT = 'off'
+            GUI.SMT = 'off'
         else:
-            SMT = 'on'
-        subprocess.run('/usr/bin/echo ' + SMT +
-                       ' > /sys/devices/system/cpu/smt/control', shell=True)
+            GUI.SMT = 'on'
+        subprocess.run('/usr/bin/echo ' + GUI.SMT +
+                       ' | sudo tee /sys/devices/system/cpu/smt/control', shell=True)
+        print("Toggled SMT")
 
     if event.ui_element == GUI.MIN_BUTTON:
         button_number = 0
@@ -152,25 +187,25 @@ def keyboard_mouse_events(event):
 
     if event.ui_element == GUI.BOOST_BUTTON:
         if subprocess.getoutput('cat /sys/devices/system/cpu/cpufreq/boost') == '1':
-            BOOST = '0'
+            GUI.BOOST = '0'
         else:
-            BOOST = '1'
-        subprocess.run('/usr/bin/echo ' + BOOST +
-                       ' > /sys/devices/system/cpu/cpufreq/boost', shell=True)
-
+            GUI.BOOST = '1'
+        subprocess.run('/usr/bin/echo ' + GUI.BOOST +
+                       ' | sudo tee /sys/devices/system/cpu/cpufreq/boost', shell=True)
+        print("Toggled Boost")
     if event.ui_element == GUI.DEFAULT_BUTTON:
         subprocess.run('/usr/bin/echo ' + DEFAULT_BOOST +
-                       ' > /sys/devices/system/cpu/cpufreq/boost', shell=True)
+                       ' | sudo tee /sys/devices/system/cpu/cpufreq/boost', shell=True)
         subprocess.run('/usr/bin/echo ' + DEFAULT_SMT +
-                       ' > /sys/devices/system/cpu/smt/control', shell=True)
+                       ' | sudo tee /sys/devices/system/cpu/smt/control', shell=True)
 
     if event.ui_element == GUI.PERFORMANCE_BUTTON:
-        PARAMS = '--max-performance'
+        PARAMS = MAX_PERFORMANCE
         subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)
         GUI.POWER_PROFILE_LABEL.set_text("Power Profile: Performance")
 
     if event.ui_element == GUI.POWERSAVER_BUTTON:
-        PARAMS = '--power-saving'
+        PARAMS = POWER_SAVER
         subprocess.run(RYZENADJ + ' ' + PARAMS, shell=True)
         GUI.POWER_PROFILE_LABEL.set_text("Power Profile: Power Saver")
 
@@ -191,18 +226,20 @@ def keyboard_mouse_events(event):
 
     if event.ui_element == GUI.POSITIVE_INCREMENT_BUTTON and GUI.currentTDP != int(MAX_TDP) // 1000:
         GUI.currentTDP = GUI.currentTDP + 1
-        PARAMS = "--stapm-limit=" + str(GUI.currentTDP * 1000) + " " + "--fast-limit=" + str(
-            GUI.currentTDP * 1000) + " " + " --slow-limit=" + str(GUI.currentTDP * 1000) + " " + " --tctl-temp=90"
+        button_number = 4
+        set_tdp_values(button_number)
 
     if event.ui_element == GUI.NEGATIVE_INCREMENT_BUTTON and GUI.currentTDP != int(MIN_TDP) // 1000:
         GUI.currentTDP = GUI.currentTDP - 1
-        PARAMS = "--stapm-limit=" + str(GUI.currentTDP * 1000) + " " + "--fast-limit=" + str(
-            GUI.currentTDP * 1000) + " " + " --slow-limit=" + str(GUI.currentTDP * 1000) + " " + " --tctl-temp=90"
+        button_number = 5
+        set_tdp_values(button_number)
 
     if event.ui_element == GUI.APPLY_CHANGES:
         print("Writing" + " " + TARGET_TDP + " " + "with Ryzenadj")
-        subprocess.run(RYZENADJ + ' ' + "--stapm-limit=" + TARGET_TDP + " " + "--fast-limit=" +
-                       TARGET_TDP + " " + "--slow-limit=" + TARGET_TDP + " " + "--tctl-temp=90", shell=True)
+        print(RYZENADJ + ' ' + STAPM_LIMIT + "=" + TARGET_TDP + " " + FAST_LIMIT + "=" +
+                       TARGET_TDP + " " + SLOW_LIMIT + "=" + TARGET_TDP + " " + TCTL_TEMP + "=" + TEMP_LIMIT)
+        subprocess.run(RYZENADJ + ' ' + STAPM_LIMIT + "=" + TARGET_TDP + " " + FAST_LIMIT + "=" +
+                       TARGET_TDP + " " + SLOW_LIMIT + "=" + TARGET_TDP + " " + TCTL_TEMP + "=" + TEMP_LIMIT, shell=True)
 
 
 def __main__():
