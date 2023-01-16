05a57bb3f7148eddc75c09178bc27eb318aafd9e
Matthew Anderson <ruinairas1992@gmail.com>
2023-01-16 11:31:08 -0600

    Fix a special error in paths

--- usr/lib/temperamental/inject-gamescope.sh
+++ usr/lib/temperamental/inject-gamescope.sh
@@ -2,5 +2,5 @@
 
 # Run as Steam Deck
 echo "Use DISPLAY=:1 if running from desktop, use DISPLAY=:0 if running from a session"
-DISPLAY=:0 /usr/lib/temperamental/./gamescope-xprops-swap /usr/lib/temperammental/temperamental.py
+DISPLAY=:0 /usr/lib/temperamental/./gamescope-xprops-swap /usr/lib/temperamental/temperamental.py
 
