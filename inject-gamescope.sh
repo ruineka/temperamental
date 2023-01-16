1aaedb10c71f0142ea7c3c6adaac5d0e4a0e456d
ruineka-server <ruineka-server@pop-os.localdomain>
2023-01-15 19:39:09 -0600

    Remove hard coded "DEFAULT" parameter in inject-gamescope

--- inject-gamescope.sh
+++ inject-gamescope.sh
@@ -2,5 +2,5 @@
 
 # Run as Steam Deck
 echo "Use DISPLAY=:1 if running from desktop, use DISPLAY=:0 if running from a session"
-DISPLAY=:0 ./gamescope-xprops-swap $PWD/temperamental.py "DEFAULT"
+DISPLAY=:0 ./gamescope-xprops-swap $PWD/temperamental.py
 
