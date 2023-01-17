19eee98bd0182f9e7078fa3389c9055aa652c0d5
Matthew Anderson <ruinairas1992@gmail.com>
2023-01-17 10:08:18 -0600

    Add VirtualBox to devices to help development

--- usr/lib/temperamental/devices.py
+++ usr/lib/temperamental/devices.py
@@ -104,6 +104,15 @@ elif system_id == "AOKZOE A1 AR07":
     QUIET_FAN_CONFIG = "/usr/share/temperamental/profiles/AOKZOE-MINIPRO-QUIET.yaml"
     BALANCED_FAN_CONFIG = "/usr/share/temperamental/profiles/AOKZOE-MINIPRO-BALANCED.yaml"
     PERF_FAN_CONFIG = "/usr/share/temperamental/profiles/AOKZOE-MINIPRO-PERF.yaml"
+elif system_id == "VirtualBox":
+    SYSTEM_NAME = system_id
+    MIN_TDP = "5000"
+    STEP_ONE = "10000"
+    STEP_TWO = '15000'
+    MAX_TDP = '28000'
+    QUIET_FAN_CONFIG = ""
+    BALANCED_FAN_CONFIG = ""
+    PERF_FAN_CONFIG = ""
 else:
     print(system_id + " " + "is not currently compatible, if you know the limitations of your hardware you can add support on your own")
     sys.exit(-1)
