458390c0683593011a8682a7ab2019a504a5d0c0
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2023-01-25 11:52:15 -0300

    Run as a module

--- temperamental/__main__.py
+++ temperamental/__main__.py
@@ -1,5 +1,9 @@
+import pyglet
+from .gui import TemperamentalMainGUI
+
 def main():
-    pass
+    main_window = TemperamentalMainGUI()
+    pyglet.app.run()
 
 if __name__ == '__main__':
     main()
